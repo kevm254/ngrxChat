bf0d976e7d1cf25b068c5a97295c24bbead5e7ee
K M <kevm2@yahoo.com>
2017-04-15 10:27:33 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
+import * as _ from 'lodash';
 
 import { AppComponent } from './app.component';
 import { UserSelectionComponent } from './user-selection/user-selection.component';
@@ -10,6 +12,32 @@ import { MessageSectionComponent } from './message-section/message-section.compo
 import { ThreadListComponent } from './thread-list/thread-list.component';
 import { MessageListComponent } from './message-list/message-list.component';
 import {ThreadsService} from "./services/threads.service";
+import { ApplicationState, INITIAL_APPLICATION_STATE } from "./store/application-state";
+import { Action } from "@ngrx/store";
+import { LOAD_USER_THREADS_ACTION, LoadUserThreadsAction } from "./store/actions";
+
+export function storeReducer(state: ApplicationState = INITIAL_APPLICATION_STATE, action: Action): ApplicationState {
+  switch(action.type) {
+    case LOAD_USER_THREADS_ACTION:
+      return handleLoadUserThreadsAction(state, action);
+    default:
+      return state;
+  }
+}
+
+function handleLoadUserThreadsAction(state: ApplicationState, action: LoadUserThreadsAction): ApplicationState {
+const userData = action.payload;
+
+const newState: ApplicationState = Object.assign({}, state);
+
+newState.storeData = {
+  participants: _.keyBy(action.payload.participants, 'id'),
+  messages: _.keyBy(action.payload.messages, 'id'),
+  threads: _.keyBy(action.payload.threads, 'id')
+};
+return newState;
+
+}
 
 @NgModule({
   declarations: [
@@ -23,7 +51,8 @@ import {ThreadsService} from "./services/threads.service";
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    StoreModule.provideStore(storeReducer)
   ],
   providers: [ThreadsService],
   bootstrap: [AppComponent]
--- src/app/thread-section/thread-section.component.ts
+++ src/app/thread-section/thread-section.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ThreadsService} from "../services/threads.service";
+import { Store } from "@ngrx/store";
+import { ApplicationState } from "../store/application-state";
+import { LoadUserThreadsAction } from '../store/actions';
 
 
 @Component({
@@ -9,15 +12,17 @@ import {ThreadsService} from "../services/threads.service";
 })
 export class ThreadSectionComponent implements OnInit {
 
-  constructor(private threadsService: ThreadsService) {
-
+  constructor(private threadsService: ThreadsService, private store: Store<ApplicationState>) {
+    store.subscribe((state) => {
+      console.log('Thread section received state', state);
+    })
   }
 
-
-
   ngOnInit() {
-
-        this.threadsService.loadUserThreads();
+        this.threadsService.loadUserThreads()
+          .subscribe((allUserData) => {
+            this.store.dispatch(new LoadUserThreadsAction(allUserData));
+          });
 
   }
 
